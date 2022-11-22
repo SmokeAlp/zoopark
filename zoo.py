2537c88f88c8027d93780ecea7b3314f023097fa
SmokeAlp <118676964+SmokeAlp@users.noreply.github.com>
2022-11-22 16:25:06 +0300

    ыуее

--- zoo.py
+++ zoo.py
@@ -4,78 +4,136 @@ import random
 
 
 class Giraffe:
+    # общее
+    animal_type = 'жираф'
+    biom = 'саванна'
+    territory = '100 км^2'
+    food = ['кустарники', 'трава', 'фрукты']
+    predator = 'травоядное'
+    sound = 'руауавуагага(не слышно человеку)'
 
     def __init__(self, a_name, a_eat, a_age):
-        # общее
-        self.Type = 'жираф'
-        self.Biom = 'саванна'
-        self.Territory = '100 км^2'
-        self.Food = ['кустарники', 'трава', 'фрукты']
-        self.Predator = 'травоядное'
-        self.Sound = 'руауавуагага(не слышно человеку)'
-
         # конкретное
-        self.Name = a_name
-        self.Eat = a_eat
-        self.Age = a_age
+        self.__name = a_name
+        self.eat = a_eat
+        self.__age = a_age, 
 
-    def eat_food(self):
-        print(self.Food[random.randint(0, 2)], 'Ом-ном-ном')
+    def eat_food(self, FoodType):
+        if FoodType in self.food:
+            print('Ом-ном-ном')
+        else:
+            print('Я не буду', FoodType)
 
     def do_noize(self):
-        print(self.Sound)
+        print(self.name, self.sound)
 
     def play(self):
-        print('Удовлетворённо', self.Sound)
+        print('Удовлетворённо', self.sound)
+        
+    @Age.setter
+    
+    def Age(self, value):
+        if value is int:
+            if value >= 0:
+                self.__age = value
+            else:
+                print('Я живой')
+    
+    @property
+    
+    def Age(self):
+        return self.__age
 
+    def Name(self):
+        return self.__name
 
 class Horse:
 
+    # общее
+    animal_type = 'лошадь'
+    biom = 'степь'
+    territory = '200 м^2'
+    food = ['сено', 'трава']
+    predator = 'травоядное'
+    sound = 'Шевелись, Плотва'
+        
     def __init__(self, a_name, a_eat, a_age):
-        # общее
-        self.Type = 'лошадь'
-        self.Biom = 'степь'
-        self.Territory = '200 м^2'
-        self.Food = ['сено', 'трава']
-        self.Predator = 'травоядное'
-        self.Sound = 'Шевелись, Плотва'
-
         # конкретное
-        self.Name = a_name
-        self.Eat = a_eat
-        self.Age = a_age
+        self.__name = a_name
+        self.eat = a_eat
+        self.__age = a_age
 
-    def eat_food(self):
-        print(self.Food[random.randint(0, 1)], 'Ом-ном-ном')
+    def eat_food(self, FoodType):
+        if FoodType in self.food:
+            print('Ом-ном-ном')
+        else:
+            print('Я не буду', FoodType)
 
     def do_noize(self):
-        print(self.Sound)
+        print(self.sound)
 
     def play(self):
-        print('Удовлетворённо', self.Sound)
+        print('Удовлетворённо', self.sound)
+        
+    @Age.setter
+    
+    def Age(self, value):
+        if value is int:
+            if value >= 0:
+                self.__age = value
+            else:
+                print('Я живой')
+    
+    @property
+    
+    def Age(self):
+        return self.__age
+
+    def Name(self):
+        return self.__name
 
 
 class Bear:
 
+    # общее
+    animal_type = 'медвед'
+    biom = 'Тайга'
+    territory = '6 км^2'
+    food = ['мясо', 'ягоды']
+    predator = 'всеядное'
+    sound = 'Ну буфета здесь нет наверное...'
+        
     def __init__(self, a_name, a_eat, a_age):
-        # общее
-        self.Type = 'медвед'
-        self.Biom = 'Россия'
-        self.Territory = '6 км^2'
-        self.Food = ['мясо', 'ягоды']
-        self.Predator = 'всеядное'
-        self.Sound = 'Ну буфета здесь нет наверное...'
-
         # конкретное
-        self.Name = a_name
-        self.Eat = a_eat
-        self.Age = a_age
+        self.__name = a_name
+        self.eat = a_eat
+        self.__age = a_age
 
-    def eat_food(self):
-        print(self.Food[random.randint(0, 1)], 'Ом-ном-ном')
+    def eat_food(self, FoodType):
+        if FoodType in self.food:
+            print('Ом-ном-ном')
+        else:
+            print('Я не буду', FoodType)
 
     def do_noize(self):
-        print(self.Sound)
+        print(self.sound)
 
     def play(self):
-        print('Удовлетворённо', self.Sound)
+        print('Удовлетворённо', self.sound)
+        
+    @Age.setter
+    
+    def Age(self, value):
+        if value is int:
+            if value >= 0:
+                self.__age = value
+            else:
+                print('Я живой')
+    
+    @property
+    
+    def Age(self):
+        return self.__age
+
+    def Name(self):
+        return self.__name
